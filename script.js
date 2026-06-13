f5b3b91b0397115b480d79d6bbe6775cd81b733e
andreykangloboyko-arch <andreykangloboyko@gmail.com>
2026-06-13 19:40:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,27 @@ let detenido = false;
 let pausado = false;
 
 async function leerPDF(){
+speechSynthesis.cancel();
 
+const prueba =
+new SpeechSynthesisUtterance(
+"Audio activado"
+);
+
+prueba.lang =
+"es-ES";
+
+speechSynthesis.speak(
+prueba
+);
+
+await new Promise(
+r=>
+setTimeout(
+r,
+1000
+)
+);
 detenido = false;
 
 const archivo =
